328cf7e95054a714eef0a0d1c6904b0c1c033ce5
Coder-the-noob <moinmatubbar6@gmail.com>
2025-09-18 18:57:15 +0600

    added so many things search box

--- script/index.js
+++ script/index.js
@@ -1,3 +1,19 @@
+const createElements = (arr) => {
+    const htmlElements = arr.map((el) => `<span class="btn">${el}</span>`);
+    return htmlElements.join(' ');
+}
+
+const manageSpinner = (status) => {
+    if(status === true ){
+        document.getElementById('spinner').classList.remove('hidden');
+        document.getElementById('word-container').classList.add('hidden');
+    }
+    else{
+        document.getElementById('word-container').classList.remove('hidden');
+        document.getElementById('spinner').classList.add('hidden');
+    }
+}
+
 const loadLessons = () => {
     const url = "https://openapi.programming-hero.com/api/levels/all";
 
@@ -6,13 +22,61 @@ const loadLessons = () => {
     .then((json) => displayLesson(json.data))
 };
 
+const removeActive = () => {
+    const lessonButtons = document.querySelectorAll('.lesson-btn');
+    lessonButtons.forEach((btn) => btn.classList.remove('active'));
+}
+
 const loadLevelWord = (id) => {
-    
+    manageSpinner(true);
     const url = `https://openapi.programming-hero.com/api/level/${id}`;
 
     fetch(url)
     .then((res) => res.json())
-    .then((json) => displayLevelWord(json.data))
+    .then((json) => {
+
+        removeActive(); // remove active class from all buttons
+        // add active class to the clicked button
+        const clickBtn = document.getElementById(`lessonn-btn-${id}`);
+        clickBtn.classList.add('active');
+        displayLevelWord(json.data);
+    } );
+};
+
+const loadwordDetail = async(id) => {
+    const url = `https://openapi.programming-hero.com/api/word/${id}`;
+
+    const res = await fetch(url);
+    const data = await res.json();
+    displayWordDetail(data.data);
+
+
+};
+
+const displayWordDetail = (word) => {
+    const detailsBox = document.getElementById('details-container');
+    detailsBox.innerHTML = `
+         <div class="">
+                <h2 class="text-2xl font-bold     "> ${word.word} (<i class="fa-solid fa-microphone-lines"></i>   :${word.pronunciation})</h2>
+            </div>
+
+            <div class="">
+                <h2 class="font-bold">Meaning</h2>
+                <p class="font-bangla">${word.meaning}</p>
+            </div>
+
+            <div class="">
+                <h2 class="font-bold">Example</h2>
+                <p class="">${word.sentence}</p>
+            </div>
+
+            <div class="">
+                <h2 class="font-bold">Synonyms</h2>
+                <div class="">${createElements(word.synonyms)}</div>
+            </div>
+    `;
+
+    document.getElementById('word_modal').showModal();
 };
 
 const displayLevelWord = (words) => {
@@ -30,6 +94,7 @@ const displayLevelWord = (words) => {
        </div>
 
         `;
+        manageSpinner(false);
         return;
 }
 
@@ -44,7 +109,7 @@ const displayLevelWord = (words) => {
             <div class="font-semibold text-2xl font-bangla">"${word.meaning ? word.meaning : 'অর্থ পাওয়া যায়নি'}/ ${word.pronunciation ? word.pronunciation : "Pronunciation পাওয়া যায়নি"}"</div>
 
             <div class="flex justify-between items-center">
-                <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button>
+                <button onclick="loadwordDetail(${word.id})" class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-circle-info"></i></button>
                 <button class="btn bg-[#1A91FF10] hover:bg-[#1A91FF80] "><i class="fa-solid fa-volume-high"></i></button>
             </div>
 
@@ -53,6 +118,7 @@ const displayLevelWord = (words) => {
 
     wordContainer.appendChild(card);
    });
+   manageSpinner(false);
 };
 
 const displayLesson = (lessons) => {
@@ -68,7 +134,7 @@ const displayLesson = (lessons) => {
         console.log(lesson);
         const btnDiv = document.createElement('div');
         btnDiv.innerHTML = `
-                <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary"
+                <button id='lessonn-btn-${lesson.level_no}' onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary lesson-btn"
                   ><i class="fa-solid fa-book-open"></i> Lesson - ${lesson.level_no}</button> 
                   `;
     //     4.append into container
@@ -79,3 +145,22 @@ const displayLesson = (lessons) => {
 };
 
 loadLessons();
+
+document.getElementById('btn-search').addEventListener('click', () => {
+
+    removeActive(); // remove active class from all buttons
+    const input = document.getElementById('input-search');
+    const searchValue = input.value.trim().toLowerCase();
+    console.log(searchValue);
+
+    fetch("https://openapi.programming-hero.com/api/words/all")
+    .then((res) => res.json())
+    .then((data) => {
+        const allWords = data.data;
+        const filterWords = allWords.filter((word) => word.word.toLowerCase().includes(searchValue));
+
+        displayLevelWord(filterWords);
+    });
+
+    
+});
